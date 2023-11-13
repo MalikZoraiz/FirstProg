c6216e7bc0eb0ea97a1b6e5e1479e2f1daac4591
MalikZoraiz <zoraizali777@gmail.com>
2023-11-12 17:03:31 -0800

    Add

---
+++ main.py
@@ -0,0 +1,12 @@
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+Age = 24
+print("Hello World ")
+print(Age)
+
+#Input Value
+
+Name = input("What is your name ")
+print("Hello " + Name)
+
