7c0fdfe81472b7ffac509b4c7ec09a1861512325
MalikZoraiz <zoraizali777@gmail.com>
2023-11-12 17:24:33 -0800

    Add

---
+++ Operators.py
@@ -0,0 +1,24 @@
+#Arithmetic Operator
+
+print(10+3)
+print(10//3)
+print(10**3)
+print(10 % 3)
+
+#Operator Precedense
+
+x = 10+3*2
+print(x)
+y = (10+3)*2
+print(y)
+
+#Comparison Operators
+
+x = 3 > 2
+y = 3 <= 2
+z = 3 == 2
+a = 1 != 2
+print (x, y, z, a)
+
+#Logical Operators
+
--- main.py
+++ main.py
@@ -10,3 +10,13 @@ print(Age)
 Name = input("What is your name ")
 print("Hello " + Name)
 
+#Type Conversion
+
+birth_year = input("Enter your birth year: ")
+Age = 2023 - int(birth_year)
+print(Age)
+
+First = 10
+Second = 20.1
+num = First + float(Second)
+print("Your num is " + str(num))
